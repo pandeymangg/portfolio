fab8a06ff505273ee4e2207b3ced1ab67f87cd4f
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-08-23 16:52:57 +0530

    experience section styles initial

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,13 +1,48 @@
 import React from "react";
+import DatePill from "../Blog/DatePill";
 
 const Experience = () => {
   return (
-    <section id="experience" className="home flex flex-col px-8 bg-white">
-      <div className="max-w-2xl flex flex-col mx-auto">
-        <div className="mb-6">
+    <section
+      id="experience"
+      className="home w-full mx-auto flex flex-col px-8 bg-white mb-8"
+    >
+      <div className="max-w-2xl flex flex-col mx-auto items-start">
+        <div className="mb-8">
           <h1 className="text-5xl font-bold tracking-tight text-black">
             Experience
           </h1>
+        </div>
+
+        <div>
+          <div className="mb-4 flex justify-between items-center">
+            <h2 className="text-3xl font-semibold text-gray-900">Quinn</h2>
+            <DatePill date={"May 2021 - August 2021"} />
+          </div>
+          <div>
+            <ul className="flex flex-col gap-2 text-base text-gray-600">
+              <li>Converted designs to responsive web pages.</li>
+              <li>
+                Learnt tailwindcss and used it to make pixel perfect UI
+                components, writing robust, ready to go to production level
+                code.
+              </li>
+              <li>
+                Built a blog for the web app. Learnt a lot about Content
+                Management Systems (CMS) and static site generation using
+                next.js. Used notion as a CMS for the blog.
+              </li>
+              <li>
+                Learnt next.js and implemented ways to make the web app more
+                performant, including optimizing images, lazy loading data,
+                preloading site resources and so on.
+              </li>
+              <li>
+                Learnt react query and implemented hooks for data fetching and
+                caching.
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -26,7 +26,7 @@ const headerLinksArray = [
 
 const Header = () => {
   return (
-    <nav className="flex items-center max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white md:my-2 bg-opacity-60 fixed top-0 inset-x-0 z-10">
+    <nav className="flex items-center max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white bg-opacity-60 fixed top-0 inset-x-0 z-10 header-backdrop">
       <div className="nav-container w-full flex justify-between items-center">
         <div className="nav-heading">
           <h1 className="text-3xl font-bold text-gray-900">Anshuman Pandey</h1>
