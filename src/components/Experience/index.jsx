4ddcbede8906f116318315a54b6e2a3b7e59d5c8
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-08-23 17:14:02 +0530

    experience section

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import DatePill from "../Blog/DatePill";
+import { BsDot } from "react-icons/bs";
+import Link from "next/link";
 
 const Experience = () => {
   return (
@@ -8,7 +10,7 @@ const Experience = () => {
       className="home w-full mx-auto flex flex-col px-8 bg-white mb-8"
     >
       <div className="max-w-2xl flex flex-col mx-auto items-start">
-        <div className="mb-8">
+        <div className="mb-16">
           <h1 className="text-5xl font-bold tracking-tight text-black">
             Experience
           </h1>
@@ -21,26 +23,71 @@ const Experience = () => {
           </div>
           <div>
             <ul className="flex flex-col gap-2 text-base text-gray-600">
-              <li>Converted designs to responsive web pages.</li>
-              <li>
-                Learnt tailwindcss and used it to make pixel perfect UI
-                components, writing robust, ready to go to production level
-                code.
-              </li>
-              <li>
-                Built a blog for the web app. Learnt a lot about Content
-                Management Systems (CMS) and static site generation using
-                next.js. Used notion as a CMS for the blog.
-              </li>
-              <li>
-                Learnt next.js and implemented ways to make the web app more
-                performant, including optimizing images, lazy loading data,
-                preloading site resources and so on.
-              </li>
-              <li>
-                Learnt react query and implemented hooks for data fetching and
-                caching.
-              </li>
+              <div className="flex gap-2 items-center">
+                <div className="text-black">
+                  <BsDot size={"2rem"} />
+                </div>
+                <li>Converted designs to responsive web pages.</li>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <div className="text-black">
+                  <BsDot size={"2rem"} />
+                </div>
+                <li>
+                  Learnt <span className="font-semibold">tailwindcss</span> and
+                  used it to make pixel perfect UI components,{" "}
+                  <span className="font-semibold">
+                    writing robust, ready to go to production level code
+                  </span>
+                  .
+                </li>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <div className="text-black">
+                  <BsDot size={"2rem"} />
+                </div>
+                <li>
+                  <span className="font-semibold">
+                    Built a blog for the web app
+                  </span>
+                  . Learnt a lot about{" "}
+                  <span className="font-semibold">
+                    Content Management Systems (CMS)
+                  </span>{" "}
+                  and{" "}
+                  <span className="font-semibold">
+                    static site generation (SSG) using next.js
+                  </span>
+                  . Used notion as a CMS for the blog.
+                </li>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <div className="text-black">
+                  <BsDot size={"2rem"} />
+                </div>
+                <li>
+                  Learnt next.js and implemented ways to make the web app more
+                  performant,{" "}
+                  <span className="font-semibold">
+                    including optimizing images, lazy loading data, preloading
+                    site resources
+                  </span>{" "}
+                  and so on.
+                </li>
+              </div>
+
+              <div className="flex gap-2 items-center">
+                <div className="text-black">
+                  <BsDot size={"2rem"} />
+                </div>
+                <li>
+                  Learnt <span className="font-semibold">react query</span> and
+                  implemented hooks for data fetching and caching.
+                </li>
+              </div>
             </ul>
           </div>
         </div>
