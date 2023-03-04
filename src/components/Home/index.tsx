299a000419c1ece63c7750e0caf4d773bf9a9a0f
anshuman9999 <anshuman@tealfeed.com>
2023-03-04 14:38:36 +0530

    added colors

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 import Image from 'next/image';
+import { Conditional } from '@pandeymangg/react-conditional';
 
 const Home = () => {
   return (
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import Head from 'next/head';
+import { Conditional } from '@pandeymangg/react-conditional';
 
 interface ILayoutProps {
   docTitle: string;
   metaDescription: string;
-  children: React.ReactElement;
+  children: React.ReactNode;
 }
 
 const Layout = ({ docTitle, metaDescription, children }: ILayoutProps) => {
   return (
-    <div className='layout bg-white dark:bg-darkBg text-gray-50 dark:text-gray-900'>
+    <div className='layout bg-bgPrimary dark:bg-darkBgPrimary text-textPrimary dark:text-darkTextPrimary'>
       <Head>
-        {docTitle && <title>{docTitle}</title>}
+        <Conditional condition={!!docTitle}>
+          <title>{docTitle}</title>
+        </Conditional>
 
-        {metaDescription && (
+        <Conditional condition={!!metaDescription}>
           <meta name='description' content={metaDescription}></meta>
-        )}
+        </Conditional>
       </Head>
+
       {children}
     </div>
   );
